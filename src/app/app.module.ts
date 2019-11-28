899a90c97b643dd5c9d20aa5ea5bba6567082d20
Sivakumar <siva@Sivakumars-MacBook-Pro.local>
2019-11-28 22:36:16 +0100

    Added user and product data as JSON

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CheaderComponent } from './cheader/cheader.component';
+import { CsidebarComponent } from './csidebar/csidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CheaderComponent
+    CheaderComponent,
+    CsidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/csidebar/csidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-csidebar',
+  templateUrl: './csidebar.component.html',
+  styleUrls: ['./csidebar.component.css']
+})
+export class CsidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
